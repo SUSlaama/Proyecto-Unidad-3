7c317b6994fed4e3059f5b52eb08613a94cef8e8
jesus <jesus@MW14Q1C>
2024-11-25 17:30:00 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto Unidad 3/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace Proyecto_Unidad_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBoxPorts = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDesconnected = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBoxReceivedData = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // comboBoxPorts
+            // 
+            this.comboBoxPorts.FormattingEnabled = true;
+            this.comboBoxPorts.Location = new System.Drawing.Point(266, 163);
+            this.comboBoxPorts.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBoxPorts.Name = "comboBoxPorts";
+            this.comboBoxPorts.Size = new System.Drawing.Size(256, 28);
+            this.comboBoxPorts.TabIndex = 0;
+            this.comboBoxPorts.SelectedIndexChanged += new System.EventHandler(this.comboBoxPorts_SelectedIndexChanged);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.BackColor = System.Drawing.Color.MediumPurple;
+            this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.Location = new System.Drawing.Point(50, 502);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(190, 94);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Conectar";
+            this.btnConnect.UseVisualStyleBackColor = false;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click_1);
+            // 
+            // btnDesconnected
+            // 
+            this.btnDesconnected.BackColor = System.Drawing.Color.MediumPurple;
+            this.btnDesconnected.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDesconnected.Location = new System.Drawing.Point(296, 502);
+            this.btnDesconnected.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnDesconnected.Name = "btnDesconnected";
+            this.btnDesconnected.Size = new System.Drawing.Size(190, 94);
+            this.btnDesconnected.TabIndex = 3;
+            this.btnDesconnected.Text = "Desconectar";
+            this.btnDesconnected.UseVisualStyleBackColor = false;
+            this.btnDesconnected.Click += new System.EventHandler(this.btnDesconnected_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(46, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(679, 64);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Practica para la Unidad 3";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(190, 102);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(411, 37);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Seleccione el puerto COM";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(298, 208);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(186, 37);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Resultados";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Crimson;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(535, 504);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 94);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Cerrar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBoxReceivedData
+            // 
+            this.richTextBoxReceivedData.Location = new System.Drawing.Point(197, 252);
+            this.richTextBoxReceivedData.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.richTextBoxReceivedData.Name = "richTextBoxReceivedData";
+            this.richTextBoxReceivedData.Size = new System.Drawing.Size(382, 202);
+            this.richTextBoxReceivedData.TabIndex = 8;
+            this.richTextBoxReceivedData.Text = "";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(784, 641);
+            this.Controls.Add(this.richTextBoxReceivedData);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDesconnected);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.comboBoxPorts);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxPorts;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDesconnected;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBoxReceivedData;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ Proyecto Unidad 3/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace Proyecto_Unidad_3
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            serialPort = new SerialPort();
+            serialPort.BaudRate = 9600;
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+            foreach (string port in SerialPort.GetPortNames())
+            {
+                comboBoxPorts.Items.Add(port);
+            }
+        }
+
+        private void btnConnect_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBoxPorts.SelectedItem != null)
+                {
+                    serialPort.PortName = comboBoxPorts.SelectedItem.ToString();
+                    serialPort.Open();
+                    MessageBox.Show("Conectado a " + serialPort.PortName);
+                }
+                else
+                {
+                    MessageBox.Show("Por favor, seleccione un puerto COM.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al conectar: " + ex.Message);
+            }
+        }
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            string inData = serialPort.ReadLine();
+
+            this.Invoke(new MethodInvoker(delegate
+            {
+                richTextBoxReceivedData.AppendText(inData + Environment.NewLine + "\n");
+
+                if (!string.IsNullOrEmpty(inData))
+                {
+                    try
+                    {
+                        string trimmedData = inData.Trim();
+                        if (trimmedData.Length == 8)
+                        {
+                            int charCode = Convert.ToInt32(trimmedData, 2);
+                            char letra = (char)charCode;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error al convertir los datos: " + ex.Message);
+                    }
+                }
+            }));
+        }
+
+        private void Form1_FormClosing_1(object sender, FormClosingEventArgs e)
+        {
+            MessageBox.Show("Usted ha salido del programa. \n" + "Puerto " + serialPort.PortName + " desconectado.");
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+        }
+
+        private void btnDesconnected_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                MessageBox.Show("Puerto " + serialPort.PortName + " desconectado.");
+                serialPort.Close();
+                richTextBoxReceivedData.Text = "";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+            this.Close();
+        }
+
+        private void comboBoxPorts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
